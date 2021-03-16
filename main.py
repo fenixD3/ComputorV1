64c6140785242567a944e58e5b4252643e0e5d49
fenixD3 <filipcev2@gmail.com>
2021-03-16 10:10:24 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import argparse
+import sys
+from parser import Parser
+
+
+def process(polynomial, isVerbose, needGraphic):
+    parser = Parser(polynomial.split(), isVerbose, needGraphic)
+    parser.parse()
+    print(parser.mPolynomialBuffer)
+
+if __name__ == '__main__':
+    argParser = argparse.ArgumentParser(description='Computor Vision V1')
+    argParser.add_argument('-v', '--verbose', action='store_true', dest='isVerbose', help='Write information about full steps')
+    argParser.add_argument('-g', '--graphic', action='store_true', dest='needGraphic', help='Show a graphic of input polynomial')
+    argParser.add_argument('polynomial', type=str, help='Input polynomial')
+    try: # Возможно забить на обработку исключений
+        args = argParser.parse_args()
+    except BaseException:
+        argParser.print_help()
+        sys.exit(1)
+    process(args.polynomial, args.isVerbose, args.needGraphic)
---
+++ parser.py
@@ -0,0 +1,9 @@
+class Parser:
+
+    def __init__(self, polynomialBuffer, isVerbose, needGraphic):
+        self.mPolynomialBuffer = polynomialBuffer
+        self.mIsVErbose = isVerbose
+        self.mNeedGraphic = needGraphic
+
+    def parse(self):
+        pass
