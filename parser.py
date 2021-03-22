521b208686ac24c5da16265244c99184be75c8ac
Denis Filiptsev <denis.filiptsev@ntprog.com>
2021-03-22 18:51:51 +0300

    parse string but not array

--- parser.py
+++ parser.py
@@ -1,5 +1,9 @@
 import re
 from helpers import print_error
+from collections import namedtuple
+
+
+token_properties = namedtuple('token_properties', 'has_digit, has_sign, has_variable, degree')
 
 
 class Parser:
@@ -8,7 +12,7 @@ class Parser:
         self.mPolynomial = polynomial
         self.mIsVerbose = isVerbose
         self.mNeedGraphic = needGraphic
-        self.mParsedPolynomial = []
+        self.mParsedPolynomial = list()
 
     def parse(self):
         if '=' not in self.mPolynomial:
@@ -17,24 +21,8 @@ class Parser:
         if self.mIsVerbose:
             print("Splitting input at left and fight side")
         left_side, right_side = self.mPolynomial.split('=')
-        left_side = left_side.split()
-        right_side = right_side.split()
+        left_side = left_side.replace(' ', '')
+        right_side = right_side.replace(' ', '')
+        print(left_side, right_side)
         if self.mIsVerbose:
             print("Process parsing input polynomial")
-
-        for idx_token in enumerate(left_side):
-            index = idx_token[0]
-            token = idx_token[1]
-            token_type = self.get_token_type(token)
-
-        for idx_token in enumerate(right_side):
-            pass
-
-    def get_token_type(self, token):
-        has_digit = re.search(r'\d+\.\d+|\d+', token)
-        has_sign = re.search(r'[\+,-,*,/]', token)
-        has_variable = re.search(r'x', token)
-        has_degree = re.search(r'\^', token)
-        if has_degree:
-            degree = token[has_degree.start(0) + 1:has_degree.start(0) + 2]
-
